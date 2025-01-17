6eb40419e6acc2e94ccc3ab179038cbb79fb1eaf
Dnyaneshwar Yadav Wagh <85550894+dnyaneshwarwagh0072@users.noreply.github.com>
2025-01-17 14:33:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 //your JS code here. If required.
+function showData() {
+    const savedUsername = localStorage.getItem('username');
+    const savedPassword = localStorage.getItem('password');
+    if (savedUsername && savedPassword) {
+        document.getElementById('existing').style.display = 'block';
+    }
+};
+
+document.getElementById('submit').onclick = function () {
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+    const rememberMe = document.getElementById('checkbox').checked;
+
+    if (rememberMe) {
+        localStorage.setItem('username', username);
+        localStorage.setItem('password', password);
+    } else {
+        localStorage.removeItem('username');
+        localStorage.removeItem('password');
+    }
+
+    alert(`Logged in as ${username}`);
+};
+
+document.getElementById('existing').onclick = function () {
+    const savedUsername = localStorage.getItem('username');
+    alert(`Logged in as ${savedUsername}`);
+};
+showData();
